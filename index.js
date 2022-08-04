c252717aba8256faa5f844270b9c3e0080f30e76
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2022-08-04 12:49:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema';
+const app = express();
+
+
+app.get('/',(req,res) =>{
+
+    res.send('GraphQL is amazing');
+});
+
+const root = {
+    product: () => {
+        return{
+            "id": 2879979,
+            "name": "widget",
+            "description": "Beautiful widget to use in your garden",
+            "price": 34.99,
+            "soldout": false
+        }
+    }
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(8080,() => console.log('Running server on port localhost:8080/grapghql'));
